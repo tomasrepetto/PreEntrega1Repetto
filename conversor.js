60208c21635f0d78095f7a6e440f5bfc15f1e3f2
tomasrepetto <tomasrepetto@gmail.com>
2023-09-05 18:19:57 -0300

    Mas operaciones

--- conversor.js
+++ conversor.js
@@ -1,8 +1,19 @@
 // Conversor de moneda
 
+// Pesos
+function pesos2(valor1, valor2){
+    const resultado = valor1 * valor2;
+    return resultado;
+}
+
 // Dolar
 function dolar(valor1, valor2){
     const resultado = valor1 / valor2;
+    return resultado;
+}
+
+function dolar2(valor1, valor2){
+    const resultado = valor1 * valor2;
     return resultado;
 }
 
@@ -12,26 +23,73 @@ function euro(valor1, valor2){
     return resultado;
 }
 
+function euro2(valor1, valor2){
+    const resultado = valor1 * valor2;
+    return resultado;
+}
+
 // Real
 function real(valor1, valor2){
     const resultado = valor1 / valor2;
     return resultado;
 }
 
-function conversor(){
-    const valor1 = parseInt(prompt("Ingrese la cantidad de pesos a convertir:"));
-    const operacion = prompt("Convertir a dolar/euro/real");
+function real2(valor1, valor2){
+    const resultado = valor1 * valor2;
+    return resultado;
+}
 
-    switch(operacion){
-        case "dolar":
-            alert("Equivale a " + parseInt(dolar(valor1, 730)) + " dolares.");
-            break;
-        case "euro":
-            alert("Equivale a " + parseInt(euro(valor1, 790)) + " euros.");
-            break;
-        case "real":
-            alert("Equivale a " + parseInt(real(valor1, 100)) + " reales.");
-            break;
+
+function conversor(){
+    const operacion1 = prompt("Que moneda desea convertir? pesos/dolares/euros/reales");
+    const valor1 = parseInt(prompt("Ingrese la cantidad de " + operacion1 + " a convertir:"));
+    const operacion2 = prompt("Convertir a pesos/dolares/euros/reales");
+
+    switch(operacion1){
+        case "pesos":
+            if (operacion2 == "dolares"){
+                alert("Equivale a " + (dolar(valor1, 725).toFixed(2)) + " dolares.")
+            } else if(operacion2 == "euros"){
+                alert("Equivale a " + (euro(valor1, 793).toFixed(2)) + " euros.");
+            } else if(operacion2 == "reales"){
+                alert("Equivale a " + (real(valor1, 100).toFixed(2)) + " reales.");
+            } else{
+                alert("Equivale a " + parseInt(valor1) + " pesos.");
+            }
+        break;
+        case "dolares":
+            if (operacion2 == "pesos"){
+                alert("Equivale a " + (pesos2(valor1, 725).toFixed(2)) + " pesos.")
+            } else if(operacion2 == "euros"){
+                alert("Equivale a " + (euro2(valor1, 0.93).toFixed(2)) + " euros.");
+            } else if(operacion2 == "reales"){
+                alert("Equivale a " + (real2(valor1, 4.97).toFixed(2)) + " reales.");
+            } else{
+                alert("Equivale a " + parseInt(valor1) + " dolares.");
+            }
+        break;
+        case "euros":
+            if (operacion2 == "pesos"){
+                alert("Equivale a " + (pesos2(valor1, 793).toFixed(2)) + " pesos.")
+            } else if(operacion2 == "dolares"){
+                alert("Equivale a " + (dolar2(valor1, 1.07).toFixed(2)) + " dolares.");
+            } else if(operacion2 == "reales"){
+                alert("Equivale a " + (real2(valor1, 5.33).toFixed(2)) + " reales.");
+            } else{
+                alert("Equivale a " + parseInt(valor1) + " euros.");
+            }
+        break;
+        case "reales":
+            if (operacion2 == "pesos"){
+                alert("Equivale a " + (pesos2(valor1, 70.36).toFixed(2)) + " pesos.")
+            } else if(operacion2 == "dolares"){
+                alert("Equivale a " + (dolar2(valor1, 0.20).toFixed(2)) + " dolares.");
+            } else if(operacion2 == "euros"){
+                alert("Equivale a " + (euro2(valor1, 0.19).toFixed(2)) + " euros.");
+            } else{
+                alert("Equivale a " + parseInt(valor1) + " reales.");
+            }
+        break;
         default:
             alert("La operacion ingresada no es valida.")
     }
